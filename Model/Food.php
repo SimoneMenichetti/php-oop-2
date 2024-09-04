5303fa74ca8565cd93e00025ec3619a8f28a9f32
Menichetti Simone <simon3menichetti95@gmail.com>
2024-09-04 14:43:21 +0200

    feat: Food.php(add class fond and exted to product )

--- Model/Food.php
+++ Model/Food.php
@@ -6,9 +6,29 @@ class Food extends Product{
     public int $weight;
 
     // richiamiamo il construct di product 
-    public function __construct(string $_name,ItemTtype $_type,Category $_category,string $_image,float $_price){
+    public function __construct(string $_name,ItemTtype $_type,Category $_category,string $_image,float $_price,string $taste,int $weight){
+
+         // utilizziamo Parent::__construct per relazionare figlio con il padre
+
+         parent::__construct($name, $type, $category, $image,$price,new ItemType('Food'));
+         $this->taste=$taste;
+         $this->weight=$weight;
 
     }
+
+    // getting
+
+    public function getTaste(){
+        return $this->taste;
+
+    }
+
+    public function getWeight(){
+        return $this->weight;
+    }
+
+   
+
 }
 
 ?>
